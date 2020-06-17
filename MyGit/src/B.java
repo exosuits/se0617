b680cc440e7a460e79925465ef71d79f3ea72c07
js <js@121.167.127.149>
2020-06-17 20:35:03 +0900

    start

---
+++ MyGit/src/B.java
@@ -0,0 +1,6 @@
+
+public class B {
+	public void main(String args[]) {
+		System.out.println("b");
+	}
+}
---
+++ MyGit/src/C.java
@@ -0,0 +1,7 @@
+
+
+public class C {
+	public void main(String args[]) {
+		System.out.println("c");
+	}
+}
